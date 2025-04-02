c48388877896bc3b5590ea348fbec3425eaf2db2
Alton <152930905+Mighty2423@users.noreply.github.com>
2025-04-02 16:59:52 -0600

    lets go

--- backend/jobliers-api/server.js
+++ backend/jobliers-api/server.js
@@ -107,8 +107,4 @@ app.get('/scams', async (req, res) => {
 
 
 const PORT = process.env.PORT || 3000;
-if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
-}
-export default app;
-
+app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
